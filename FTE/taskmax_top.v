557fe35160813770bd577eacea430bf9716fe953
Ernaut <44167659+Ciftcierenv@users.noreply.github.com>
2023-10-22 00:47:31 +0300

    Update taskmax_top.v

--- FTE/taskmax_top.v
+++ FTE/taskmax_top.v
@@ -1,29 +1,28 @@
 `timescale 1ns/1ps
 
 module taskmaxtop (
+
+output wire[7:0] out
 );
 reg[7:0] a,b;
-reg [7:0]out;
 
 taskmax comp(
 .a(a),
 .b(b),
-.out(outreg)
+.out(out)
 );
 
 initial begin
-out = 8'b0;
+
 a= 8'b01010101;
 b= 8'b01100000;
 
-
-$display("%d",out);
 #20
 
 a= 8'b10010010;
 b= 8'b10001000;
 
-$display("%d",out);
+#20
 $finish;
 end
 
